e5e71ef400332666c64eba4255c397ed5c88b604
shubhamsomwanshi11 <shubhamsomwanshi11@gmail.com>
2026-06-08 19:48:47 +0530

    Day 16

---
+++ Day_16/FileOperations.py
@@ -0,0 +1,14 @@
+# Opening a File
+# Syntax
+# open(filename, mode)
+# Example:
+
+# Opening a file
+file = open("example.txt","r")
+
+# reading the data
+content = file.read()
+print(content)
+# close file
+file.close()
+
